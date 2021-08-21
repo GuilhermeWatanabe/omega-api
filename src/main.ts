b844db1cc1f8fb4141298be26366611640fc9b20
Guilherme Watanabe <watanab430@gmail.com>
2021-08-21 09:35:51 -0300

    Fix: cors.

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '',
-  });
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
